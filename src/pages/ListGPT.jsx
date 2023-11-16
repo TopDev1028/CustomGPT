a853a8751db4a90e3ef145983280b95eb74a55fb
TopDev1028 <app.seed5406@gmail.com>
2023-11-16 13:21:15 +0900

    icon

--- src/pages/ListGPT.jsx
+++ src/pages/ListGPT.jsx
@@ -264,7 +264,7 @@ const ListGPT = () => {
           )}
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-[50px] mt-[-20px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[40px] mx-[50px] mt-[-20px]">
         {gpts.map((gpt, index) => (
           <div
             key={index}
